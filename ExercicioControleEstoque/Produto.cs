e397753ef9eaae42bcc47bb33a574cc42c311164
Denison TS <106643117+denisonts@users.noreply.github.com>
2022-08-12 01:41:07 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioControleEstoque/Produto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ExercicioControleEstoque
+{
+    internal class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+        public int Entrada;
+
+
+        public double ValorTotalEmEstoque() {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProduto(int quantidade) {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProduto(int quantidade2) {
+            Quantidade -= quantidade2;   
+        }
+        public override string ToString()
+        {
+            return Nome 
+                + ", $ " 
+                + Preco.ToString("F2", CultureInfo.InvariantCulture) 
+                + ", " + Quantidade 
+                + " unidades, Total: $" + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }    
+}
---
+++ ExercicioControleEstoque/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+
+namespace ExercicioControleEstoque
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Produto item = new Produto();
+            
+            Console.WriteLine("Entre os dados do produto:");
+            Console.Write("Nome: ");
+            item.Nome = Console.ReadLine();
+                          
+            Console.Write("Preço: ");
+            item.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantidade no estoque: ");
+            item.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Dados Atualizados: " + item);
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+
+            Console.WriteLine();
+            int qte = int.Parse(Console.ReadLine());
+            item.AdicionarProduto(qte);
+            Console.WriteLine("Dados Atualizados: " + item);
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser removido ao estoque: ");
+
+            Console.WriteLine();
+            int qte2 = int.Parse(Console.ReadLine());
+            item.RemoverProduto(qte2);
+            Console.WriteLine("Dados Atualizados: " + item);
+
+
+        }
+    }
+}
